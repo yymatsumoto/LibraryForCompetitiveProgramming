a1fdd3c6e233dab0936cf92cf6f3cdb17aa53ba7
Yuya Matsumoto <y.matsu.moto95@gmail.com>
2020-03-10 13:03:30 +0900

    プロジェクト ファイルを追加します。

---
+++ LibrariesForCompetitiveProgramming/UnionFind.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrariesForCompetitiveProgramming
+{
+    class UnionFind
+    {
+        public int[] ParentId { get; set; }
+
+        public UnionFind(int nodeNumber)
+        {
+            this.ParentId = new int[nodeNumber];
+            for (var i = 0; i < nodeNumber; i++)
+            {
+                this.ParentId[i] = -1;
+            }
+        }
+
+        public int FindParentId(int id)
+        {
+            if (this.ParentId[id] < 0)
+            {
+                return id;
+            }
+            this.ParentId[id] = this.FindParentId(this.ParentId[id]);
+            return this.ParentId[id];
+        }
+
+        public int GetMenberSize(int id)
+        {
+            var parentId = this.FindParentId(id);
+            return -this.ParentId[parentId];
+        }
+
+        public bool IsSameGroup(int x, int y)
+        {
+            return this.FindParentId(x) == this.FindParentId(y);
+        }
+
+        public void UniteMembers(int x, int y)
+        {
+            var xParentId = this.FindParentId(x);
+            var yParentId = this.FindParentId(y);
+
+            if (xParentId == yParentId)
+            {
+                return;
+            }
+
+            if (this.ParentId[xParentId] < this.ParentId[xParentId])
+            {
+                var tmp = xParentId;
+                xParentId = yParentId;
+                yParentId = tmp;
+            }
+
+            this.ParentId[xParentId] += this.ParentId[yParentId];
+            this.ParentId[yParentId] = xParentId;
+        }
+    }
+}
