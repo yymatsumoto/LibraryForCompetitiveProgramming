0c4049b98947519693a4116f57347c25d44c89f4
Yuya Matsumoto <y.matsu.moto95@gmail.com>
2020-03-28 02:53:39 +0900

    LowerBound

--- LibraryForCompetitiveProgramming/BinarySearchTree.cs
+++ LibraryForCompetitiveProgramming/BinarySearchTree.cs
@@ -34,5 +34,37 @@ namespace LibraryForCompetitiveProgramming
         }
 
 
+        ///<summary>昇順リストと値を渡すとその値が最初に出てくるインデックスを返す</summary>
+        public static int LowerBound<T>(List<T> list, T value)
+        {
+            return LowerBound(list, value, Comparer<T>.Default);
+        }
+
+        ///<summary>昇順リストと値を渡すとその値が最初に出てくるインデックスを返す</summary>
+        public static int LowerBound<T>(List<T> list, T value, Comparer<T> comparer)
+        {
+            var head = 0;
+            var tail = list.Count - 1;
+
+            //headがtailと同じ値になるタイミングがあり、そこでheadがtail+1になって出られる
+            while (head <= tail)
+            {
+                var middle = head + (tail - head) / 2;
+
+                switch (comparer.Compare(list[middle], value))
+                {
+                    case 1:
+                        tail = middle - 1;
+                        break;
+                    case -1:
+                        head = middle + 1;
+                        break;
+                    case 0:
+                        tail = middle - 1;
+                        break;
+                }
+            }
+            return head;
+        }
     }
 }
